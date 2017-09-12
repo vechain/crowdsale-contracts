944159d13556f06e83d8f62aea2e70793aac5d11
cola tin <cola.tin.com@gmail.com>
2017-09-12 10:47:33 +0800

    reformat code, remove totalReturnedCredit

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -19,18 +19,18 @@ contract Exchange is Owned {
     uint256 public tokenQuota = 402500 ether;
 
     // quota of ether for every account that can be exchanged to token
- //   uint256 public etherQuota = 100 ether;
+    // uint256 public etherQuota = 100 ether;
 
     bool public tokenToEtherAllowed = true;
-//    bool public etherToTokenAllowed = false;
+    // bool public etherToTokenAllowed = false;
 
-    uint256 public totalReturnedCredit;             //returned ven  
+    // uint256 public totalReturnedCredit;             //returned ven  
 
 
-//    struct QuotaUsed {
-//        uint128 tokens;
-//        uint128 ethers;
-//    }
+    // struct QuotaUsed {
+    //     uint128 tokens;
+    //     uint128 ethers;
+    // }
     mapping(address => uint256) accountQuotaUsed;
 
     function Exchange() {
@@ -54,28 +54,28 @@ contract Exchange is Owned {
         return accountQuotaUsed[_account];
     }
 
-//tested
+    //tested
     function setRate(uint256 _rate) onlyOwner {
         rate = _rate;
     }
 
-//tested
+    //tested
     function setTokenQuota(uint256 _quota) onlyOwner {
         tokenQuota = _quota;
     }
 
-//    function setEtherQuota(uint256 _quota) onlyOwner {
-//        etherQuota = _quota;
-//    }
+    // function setEtherQuota(uint256 _quota) onlyOwner {
+    //     etherQuota = _quota;
+    // }
 
-//tested
+    //tested    
     function setTokenToEtherAllowed(bool _allowed) onlyOwner {
         tokenToEtherAllowed = _allowed;
     }
 
-//    function setEtherToTokenAllowed(bool _allowed) onlyOwner {
-//        etherToTokenAllowed = _allowed;
-//    }
+    // function setEtherToTokenAllowed(bool _allowed) onlyOwner {
+    //     etherToTokenAllowed = _allowed;
+    // }
 
     function receiveApproval(address _from, uint256 _value, address /*_tokenContract*/, bytes /*_extraData*/) {
         exchangeTokenToEther(_from, _value);
@@ -106,7 +106,7 @@ contract Exchange is Owned {
         require(etherAmount > 0);
         _from.transfer(etherAmount);
 
-        totalReturnedCredit+=_tokenAmount;
+        // totalReturnedCredit+=_tokenAmount;
 
         onExchangeTokenToEther(_from, _tokenAmount, etherAmount);
     }
@@ -114,23 +114,23 @@ contract Exchange is Owned {
 
     //exchange EtherToToken放到fallback函数中
     //TokenToEther
-//    function exchangeEtherToToken() payable {
-//       require(etherToTokenAllowed);
-//        require(!isContract(msg.sender));
-//
-//        uint256 quota = etherQuota.sub(accountQuotaUsed[msg.sender].ethers);
+    //    function exchangeEtherToToken() payable {
+    //       require(etherToTokenAllowed);
+    //        require(!isContract(msg.sender));
+    //
+    //        uint256 quota = etherQuota.sub(accountQuotaUsed[msg.sender].ethers);
 
-//        uint256 etherAmount = msg.value;
-//        require(etherAmount >= 0.01 ether && etherAmount <= quota);
-//        
-//        uint256 tokenAmount = etherAmount * rate;
+    //        uint256 etherAmount = msg.value;
+    //        require(etherAmount >= 0.01 ether && etherAmount <= quota);
+    //        
+    //        uint256 tokenAmount = etherAmount * rate;
 
-//        accountQuotaUsed[msg.sender].ethers = etherAmount.add(accountQuotaUsed[msg.sender].ethers).toUINT128();
+    //        accountQuotaUsed[msg.sender].ethers = etherAmount.add(accountQuotaUsed[msg.sender].ethers).toUINT128();
 
-//        require(token.transfer(msg.sender, tokenAmount));
+    //        require(token.transfer(msg.sender, tokenAmount));
 
-//        onExchangeEtherToToken(msg.sender, tokenAmount, etherAmount);                                                        
-//    }
+    //        onExchangeEtherToToken(msg.sender, tokenAmount, etherAmount);                                                        
+    //    }
 
     function isContract(address _addr) constant internal returns(bool) {
         uint size;
